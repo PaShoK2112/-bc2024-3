dbf0c4f62f669372630dc709c5c9148960192fc1
PaShoK2112 bc2024-3 <pashababiy000@gmail.com>
2024-09-30 00:38:18 +0300

    third commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const {program} = require(`commander`);
+const fs = require (`fs`);
+
+program
+.requiredOption (`-i, --input <path>`)
+.option ('-o, --output <path>')
+.option ('-d, --display');
+
+program.parse();
+
+const options = program.opts();
+if (!options.input) {
+    console.error ("Please, specify input file");
+}
+
+if (!fs.existsSync(options.input)) {
+    console.error('Cannot find input file');
+  }
+  
+  const inputData = fs.readFileSync(options.input, 'utf8');
+  
+  if (options.display) {
+    console.log('Input Data:', inputData);
+  }
+  
+  if (options.output) {
+    fs.writeFileSync(options.output, inputData);
+  }
