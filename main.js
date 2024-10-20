cb83fa0e9a92933bc849016dc1ef75dd9ba2e9ad
PaShoK2112 bc2024-3 <pashababiy000@gmail.com>
2024-10-21 00:49:58 +0300

    fourth commit

--- main.js
+++ main.js
@@ -1,28 +1,56 @@
-const {program} = require(`commander`);
-const fs = require (`fs`);
+const { program } = require('commander');
+const fs = require('fs');
 
 program
-.option (`-i, --input <path>`)
-.option ('-o, --output <path>')
-.option ('-d, --display');
+  .option('-i, --input <path>', 'Input file path')
+  .option('-o, --output <path>', 'Output file path')
+  .option('-d, --display', 'Display input data');
 
 program.parse();
 
 const options = program.opts();
+
+
 if (!options.input) {
-    console.error ("Please, specify input file");
+  console.error("Please, specify input file");
+  process.exit(1);
 }
 
 if (!fs.existsSync(options.input)) {
-    console.error('Cannot find input file');
-  }
-  
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+
+try {
+
   const inputData = fs.readFileSync(options.input, 'utf8');
-  
+  const jsonData = JSON.parse(inputData);
+
+
+  let minValue = Infinity;
+  let minAsset = null;
+
+  jsonData.forEach(asset => {
+    if (asset.value < minValue) {
+      minValue = asset.value;
+      minAsset = asset;
+    }
+  });
+
+
+  const result = `${minAsset.txt}:${minAsset.value}`;
+
+ 
   if (options.display) {
-    console.log('Input Data:', inputData);
+    console.log(result);
   }
-  
+
+
   if (options.output) {
-    fs.writeFileSync(options.output, inputData);
+    fs.writeFileSync(options.output, result);
+  } else {
+    console.log(result);
   }
+} catch (error) {
+  console.error('Error reading or processing file:', error);
+}
